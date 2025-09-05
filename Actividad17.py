adf98473d23869507dcd3b6eaf89ea4d33aa5dc5
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-09-05 17:33:34 -0600

    correcciones y mejora en la posicion de los botones

--- Actividad17.py
+++ Actividad17.py
@@ -8,20 +8,18 @@ ventana.config(bg = "#1e1e1e")
 etiqueta0 = tk.Label(ventana, text="Calculadora python",font=("Arial", 20,"bold italic"),bg="#1e1e1e",fg="white")
 etiqueta0.place(x=150, y=10)
 
-etiqueta = tk.Label(ventana, text="Escriba el primer numero ",font=("Arial", 10,"bold italic"),bg="#1e1e1e",fg="white")
+etiqueta = tk.Label(ventana, text="Escriba el primer numero ",relief ="flat",font=("Arial", 10,"bold italic"),bg="#1e1e1e",fg="white")
 etiqueta.place(x=20, y=60)
 entrada = tk.Entry(ventana,bg="#abc5e0", fg="black", relief="flat",highlightbackground="#3a3a3a", highlightcolor="#007acc", highlightthickness=2)
 entrada.place(x=200, y=60, width=150)
 
-etiqueta2 = tk.Label(ventana, text="Escriba el segundo numero ",font=("Arial", 10,"bold italic"),bg="#1e1e1e",fg="white")
+etiqueta2 = tk.Label(ventana, text="Escriba el segundo numero ",relief ="flat",font=("Arial", 10,"bold italic"),bg="#1e1e1e",fg="white")
 etiqueta2.place(x=20, y=100)
 entrada2 = tk.Entry(ventana,bg="#abc5e0", fg="black", relief="flat",highlightbackground="#3a3a3a", highlightcolor="#007acc", highlightthickness=2)
 entrada2.place(x=200, y=100, width=150)
 
-etiquetaR = tk.Label(ventana, text="Resultado ",font=("Arial", 10,"bold italic"),bg="#1e1e1e",fg="white")
-etiquetaR.place(x=20, y=140)
 etiquetaR = tk.Label(ventana, text="Resultado ",relief ="raised",font=("Arial", 10,"bold italic"),bg="#1e1e1e",fg="white")
-etiquetaR.pack(pady=5)
+etiquetaR.place(x=20, y=140)
 
 etiqueta3 = tk.Label(ventana,font=("Arial", 10,"bold italic"),bg="#1e1e1e",fg="white")
 etiqueta3.place(x=200, y=140)
@@ -81,7 +79,7 @@ class Limpiar:
 operaciones_matematicas = OperacionesMatematicas(ventana, entrada, entrada2)
 limpiar_ventana = Limpiar(ventana, entrada, entrada2)
 
-etiquetaP = tk.Label(ventana, text="Operaciones ",font=("Arial", 10,"bold italic"),bg="#1e1e1e",fg="white")
+etiquetaP = tk.Label(ventana, text="Operaciones ",relief ="raised",font=("Arial", 10,"bold italic"),bg="#1e1e1e",fg="white")
 etiquetaP.place(x=20, y=180)
 
 boton_sumar = tk.Button(ventana, text="Sumar", command=operaciones_matematicas.calcular_suma, relief ="raised",bd = 3,activebackground ="grey",width=8, height=1,activeforeground="white",font=("Arial", 10,"bold italic"))
